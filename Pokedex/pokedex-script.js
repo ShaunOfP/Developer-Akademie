f9ecc2a2e570a07ebe0911c9181a78f0f467e3e5
ShaunOfP <svenroesgen@gmail.com>
2023-10-06 20:31:56 +0200

    added new function

--- Pokedex/pokedex-script.js
+++ Pokedex/pokedex-script.js
@@ -1,7 +1,8 @@
+let howManyPokemon = 6;
 let allPokemon = [];
 let allPokemonFixedFirst = [];
 let pokemonData = [];
-
+let allPokemonData = [];
 
 async function initMain() {
     await loadAllPokemon();
@@ -12,7 +13,7 @@ async function initMain() {
 
 async function loadAllPokemon() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=6"); // zu 1010 ändern
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${howManyPokemon}`); // zu 1010 ändern
         const data = await response.json();
         allPokemon = pokemonNames = data.results.map((pokemon) => pokemon.name); //Filtert die ergebnisse aus der data Variablen nach den Namen und speichert diese in einem neuen Array (allPokemon)
         return;
@@ -47,7 +48,7 @@ async function renderPokemonInfo() {
         let pokeImage = pokeInfo[2];
 
         if (pokeTypeLength == 1) {
-        pokeContainer.innerHTML += `
+            pokeContainer.innerHTML += `
             <div class="poke-Info-Card" onclick="clicked(${j})">
                 <div class="pokemon-card-header">
                     ${allPokemonFixedFirst[j]}
@@ -75,17 +76,18 @@ async function renderPokemonInfo() {
             `;
         }
     }
+    pushDataToArray();
 }
 
 
-async function getPokemonInfo(currentPokemon) {
-    await loadPokemonData(currentPokemon);
+async function getPokemonInfo(getCurrentPokemonInfo) {
+    await loadPokemonData(getCurrentPokemonInfo);
 
     let pokeTypeLength = pokemonData['types'].length;
     let pokeImage = pokemonData['sprites']['front_default'];
     let pokeType = [];
 
-    if (pokeTypeLength == 1){
+    if (pokeTypeLength == 1) {
         pokeType = pokemonData['types'][0]['type']['name'];
         return [pokeTypeLength, pokeType, pokeImage];
     } else {
@@ -95,15 +97,46 @@ async function getPokemonInfo(currentPokemon) {
 }
 
 
-async function loadPokemonData(currentPokemon){
-    let url = `https://pokeapi.co/api/v2/pokemon/${currentPokemon}`;
+async function loadPokemonData(currentPokemonName) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${currentPokemonName}`;
     let response = await fetch(url);
     pokemonData = await response.json();
     return;
 }
 
 
-async function clicked(id){
+async function pushDataToArray() {
+    for (let x = 0; x < allPokemon.length; x++) {
+        let pokemonName = allPokemon[x];
+        let pokemonID = pokemonData['id'];
+        //pokeTypeLength einfügen
+        //pokeImage einfügen
+        //pokeType einfügen
+
+
+        for (let s = 1; s < 11; s++) {
+            let url = `https://pokeapi.co/api/v2/pokemon-color/${s}/`;
+            let colorResponse = await (await fetch(url)).json();
+            let colorLength = colorResponse['pokemon_species'].length;
+
+            for (let d = 0; d < colorLength; d++) {
+                if (colorResponse['pokemon_species'][d]['name'] == pokemonName) {
+                    let dataFeed = {
+                        'name': pokemonName,
+                        'id': pokemonID,
+                        'color': colorResponse['name']
+                    };
+
+                    allPokemonData.push(dataFeed);
+                }
+            }
+        }
+
+    }
+}
+
+
+async function clicked(id) {
     document.getElementById('pokemonContainer').classList.add('dp-none');
     document.getElementById('main-headline').classList.add('dp-none');
     document.getElementById('pokedexContainer').classList.remove('dp-none');
